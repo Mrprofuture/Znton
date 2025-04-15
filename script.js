a8f12c5de84269e07a38c4837c3175e231ade39a
Mrprofuture <mrprofuture@gmail.com>
2025-04-16 03:07:31 +0500

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const cartButtons = document.querySelectorAll(".add-to-cart");
-  
-  cartButtons.forEach(button => {
-    button.addEventListener("click", () => {
-      alert("Item added to cart!");
-    });
-  });
-});
